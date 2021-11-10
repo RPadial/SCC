b709cd5b02a75bdc3f5a1e78ef7871ab72a67b6a
RPadial <70911219+RPadial@users.noreply.github.com>
2021-11-10 19:31:27 +0100

    Included comments in SCCGU.py, Removed junk code. GUI layout size defined.

--- SCCGUI.py
+++ SCCGUI.py
@@ -11,28 +11,39 @@
 
 from PyQt5 import QtCore, QtGui, QtWidgets
 
+##########################################################################################
+#                                         DEFINES                                        #
+##########################################################################################
+Window_x_size = 1000
+Window_y_size = 350
+
+##########################################################################################
+#                                       END DEFINES                                      #
+##########################################################################################
+
 class Ui_MainWindow(object):
     def setupUi(self, MainWindow):
+        #See Doc/GUI.svg
+
+        #Creates a widget with a gridLayout. https://doc.qt.io/qt-5/qmainwindow.html
         MainWindow.setObjectName("MainWindow")
-        MainWindow.resize(1000, 350)
+        MainWindow.resize(Window_x_size, Window_y_size)
         self.centralwidget = QtWidgets.QWidget(MainWindow)
         self.centralwidget.setObjectName("centralwidget")
         self.gridLayout = QtWidgets.QGridLayout(self.centralwidget)
         self.gridLayout.setObjectName("gridLayout")
 
-        #self.page = QtWidgets.QWidget()
-        #self.page.setObjectName("page")
-        
-        #self.gridLayout_2 = QtWidgets.QGridLayout(self.centralwidget)
-        #self.gridLayout_2.setObjectName("gridLayout_2")
+        #Creates a GraphicsView. https://doc.qt.io/qt-5/graphicsview.html to visualize the images. 
         self.graphicsView = GraphicsView(self.centralwidget)
         self.graphicsView.setObjectName("graphicsView")
         self.gridLayout.addWidget(self.graphicsView, 1, 0, 1, 0)
         #self.gridLayout_2.resize(950,25)
         
+        #Add blank space in row 4 column 3 of the gridLayout
         spacerItem = QtWidgets.QSpacerItem(40, 20, QtWidgets.QSizePolicy.Expanding, QtWidgets.QSizePolicy.Minimum)
         self.gridLayout.addItem(spacerItem, 4, 3, 1, 1)
-        
+
+        #Add tittle in row 0 column 0 of the gridLayout
         self.titleLabel = QtWidgets.QLabel(self.centralwidget)
         font = QtGui.QFont()
         font.setPointSize(12)
@@ -41,70 +52,67 @@ class Ui_MainWindow(object):
         self.titleLabel.setObjectName("titleLabel")
         self.gridLayout.addWidget(self.titleLabel, 0, 0, 1, 0)
 
-        '''
-        self.label = QtWidgets.QLabel(self.centralwidget)
-        self.label.setObjectName("label")
-        self.gridLayout.addWidget(self.label, 2, 0, 1, 1)
-        '''
+        #Creates horizontal layout 1 in row 2 column 1 of gridLayout:
+        # - Position 1: labelTemp
+        # - Position 2: tempSpin
+        # - Position 3: labelC
+        # - Position 4: TempBtn
         self.horizontalLayout = QtWidgets.QHBoxLayout()
         self.horizontalLayout.setObjectName("horizontalLayout")
 
         self.labelTemp = QtWidgets.QLabel(self.centralwidget)
         self.labelTemp.setObjectName("labelTemp")
         self.horizontalLayout.addWidget(self.labelTemp)
-
+ 
         self.tempSpin = QtWidgets.QDoubleSpinBox(self.centralwidget)
         self.tempSpin.setButtonSymbols(QtWidgets.QAbstractSpinBox.NoButtons)
         self.tempSpin.setProperty("value", 36)
         self.tempSpin.setObjectName("tempSpin")
         self.horizontalLayout.addWidget(self.tempSpin)
-
+ 
         self.labelC = QtWidgets.QLabel(self.centralwidget)
         self.labelC.setObjectName("label")
         self.horizontalLayout.addWidget(self.labelC)
 
-        self.TempBtn = QtGui.QPushButton('Show Temperature')
+        self.TempBtn = QtWidgets.QPushButton('Show Temperature')
         self.TempBtn.setObjectName("TempBtn")
         self.TempBtn.setCheckable(True)
         self.horizontalLayout.addWidget(self.TempBtn)
 
-        self.AreaBtn = QtGui.QPushButton('Update Area')
+        self.AreaBtn = QtWidgets.QPushButton('Update Area')
         self.AreaBtn.setObjectName("AreaBtn")
         self.AreaBtn.setCheckable(False)
         self.AreaBtn.setEnabled(False)
         self.horizontalLayout.addWidget(self.AreaBtn)
 
-        self.horizontalLayout_2 = QtWidgets.QHBoxLayout()
-        self.horizontalLayout_2.setObjectName("horizontalLayout")
-
-        
-        '''
-
-        self.labelArea = QtWidgets.QLabel(self.centralwidget)
-        self.labelArea.setObjectName("label")
-        self.gridLayout.addWidget(self.labelArea, 3, 0, 1, 1)
-        '''
-
         self.gridLayout.addLayout(self.horizontalLayout, 2, 0, 1, 1)
+
+        #Creates horizontal layout 2 in row 3 column 1 of gridLayout:
+        # - Position 1: labelArea
+        # - Position 2: labelAreaValue
+        # - Position 3: Showbtn
+        self.horizontalLayout_2 = QtWidgets.QHBoxLayout()
+        self.horizontalLayout_2.setObjectName("horizontalLayout")        
 
         self.labelArea = QtWidgets.QLabel(self.centralwidget)
         self.labelArea.setObjectName("labelArea")
         self.horizontalLayout_2.addWidget(self.labelArea)
+
         self.labelAreaValue = QtWidgets.QLabel(self.centralwidget)
         self.labelAreaValue.setObjectName("labelAreaValue")
         self.horizontalLayout_2.addWidget(self.labelAreaValue)
 
-        self.Showbtn = QtGui.QPushButton('Show Area')
+        self.Showbtn = QtWidgets.QPushButton('Show Area')
         self.Showbtn.setObjectName("Showbtn")
         self.Showbtn.setCheckable(True)
         self.horizontalLayout_2.addWidget(self.Showbtn)
 
         self.gridLayout.addLayout(self.horizontalLayout_2, 3, 0, 1, 1)
 
-        
+        #Set QWidget centralwidget as central widget of MainWindow
         MainWindow.setCentralWidget(self.centralwidget)
 
-
+        #Set text values
         self.retranslateUi(MainWindow)
 
         QtCore.QMetaObject.connectSlotsByName(MainWindow)
